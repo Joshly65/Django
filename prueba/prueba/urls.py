a5d19af441b0c6ab9d4801c89a6d662e85de2a71
Joshly65 <johassonparra65@gmail.com>
2023-01-30 19:33:39 -0500

    urls

--- prueba/prueba/urls.py
+++ prueba/prueba/urls.py
@@ -17,5 +17,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('',include('libros.urls')),
+    path('autor/',include('autores.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
